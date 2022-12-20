e1f65475219eac9d6faa1abdd0bfb9924ce9af6d
proloy <xtrmproloy875@gmail.com>
2022-12-20 10:16:18 +0600

    Drawer Solved

--- app/src/main/java/com/amtech/oasis/ui/mainscreen/fragment/DashBoardFragment.java
+++ app/src/main/java/com/amtech/oasis/ui/mainscreen/fragment/DashBoardFragment.java
@@ -1,7 +1,9 @@
 package com.amtech.oasis.ui.mainscreen.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
@@ -9,15 +11,19 @@ import androidx.navigation.Navigation;
 
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.amtech.oasis.R;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.Objects;
 
-public class DashBoardFragment extends Fragment implements View.OnClickListener {
+
+public class DashBoardFragment extends Fragment{
 
     private CardView cardCompleted,cardPending,cardAllAssigned;
     private DrawerLayout drawerLayout ;
@@ -31,7 +37,7 @@ public class DashBoardFragment extends Fragment implements View.OnClickListener 
         View view = inflater.inflate(R.layout.fragment_dash_board, container, false);
 
         init(view);
-//        navigationView.bringToFront();
+        navigationView.bringToFront();
 
         return view;
     }
@@ -40,9 +46,11 @@ public class DashBoardFragment extends Fragment implements View.OnClickListener 
         cardCompleted = view.findViewById(R.id.cardCompleted);
         cardPending = view.findViewById(R.id.cardPending);
         cardAllAssigned = view.findViewById(R.id.cardAllAssigned);
-//        drawerLayout = view.findViewById(R.id.drawerLayout);
-//        navigationView = view.findViewById(R.id.navigationDrawer);
-//        imvDrawer = view.findViewById(R.id.imvDrawer);
+        drawerLayout = view.findViewById(R.id.drawerLayout);
+        navigationView = view.findViewById(R.id.navigationDrawer);
+        imvDrawer = view.findViewById(R.id.imvDrawer);
+
+        navigationView.setItemIconTintList(null);
 
         cardCompleted.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,14 +72,39 @@ public class DashBoardFragment extends Fragment implements View.OnClickListener 
                 Navigation.findNavController(v).navigate(R.id.action_dashBoardFragment_to_allAssignedFragment);
             }
         });
-    }
 
-    @Override
-    public void onClick(View view) {
+        imvDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
 
-        if(view == imvDrawer)
-        {
-            drawerLayout.openDrawer(Gravity.LEFT);
-        }
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                switch (id){
+                    case R.id.dn_dashboard:
+                        drawerLayout.close();
+                        break;
+
+                    case R.id.dn_stores:
+                        Navigation.findNavController(requireView()).navigate(R.id.action_dashBoardFragment_to_allAssignedFragment);
+                        break;
+
+                    case R.id.dn_task:
+
+                        break;
+
+                    case R.id.dn_signout:
+
+                        break;
+
+                }
+                return false;
+            }
+        });
     }
 }
